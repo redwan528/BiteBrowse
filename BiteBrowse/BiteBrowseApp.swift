44bce5f1447a56ebbd2b6752c5bd7bb356a9d294
redwan528 <redwankhan144@gmail.com>
2024-04-18 21:04:15 -0400

    Initial Commit

---
+++ BiteBrowse/BiteBrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteBrowseApp.swift
+//  BiteBrowse
+//
+//  Created by Redwan Khan on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BiteBrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
